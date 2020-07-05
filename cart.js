f05d73a9904ea39877663fb18863b927fb4db5d0
vidhathat <44058606+vidhathat@users.noreply.github.com>
2020-07-05 15:13:02 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
+   document.body.style.backgroundColor = "rgba(0,0,0,0.8)";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+
+  } 
